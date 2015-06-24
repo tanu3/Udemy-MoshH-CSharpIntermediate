b985627fe214233d9e6bca15b349f8f65fe3209d
tanu3 <tanu3@aol.com>
2015-06-24 18:25:22 -0400

    Lecture 14 Exercises - Post

---
+++ Section2StackOverflowPost/Post.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section2StackOverflowPost
+{
+    class Post
+    {
+        private String title;
+        private String description;
+        private DateTime creationDate;
+        private int currentVoteValue = 0;
+
+        public String Title 
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public String Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public DateTime CreationDate 
+        { 
+            get { return creationDate; } 
+        }
+        public int CurrentVoteValue 
+        {
+            get { return currentVoteValue; }
+        }
+
+        public Post()
+        {
+            this.creationDate = DateTime.Now;
+        }
+
+        public Post(String title, String description) : this()
+        {
+            this.Title = title;
+            this.Description = description;
+        }
+                
+        public int UpVote()
+        {
+            return this.currentVoteValue++;
+        }
+
+        public int DownVote()
+        {
+            if (this.currentVoteValue == 0)
+            {
+                throw new InvalidOperationException("Can't downvote a post that is at vote value of zero");
+            }
+
+            return this.currentVoteValue--;
+        }
+
+        public void PrintPost()
+        {
+            Console.WriteLine("Values before up and down voting:");
+            Console.WriteLine("Title: {0}", this.Title);
+            Console.WriteLine("Description: {0} ", this.Description);
+            Console.WriteLine("Current vote value: {0}", this.CurrentVoteValue);
+            Console.ReadLine();
+        }
+       
+    
+
+    }
+}
---
+++ Section2StackOverflowPost/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section2StackOverflowPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newPost = new Post();
+
+            newPost.Title = "My First Post";
+            newPost.Description = "This post exists to test our knowledge of encapsulation in OOP";
+
+            newPost.PrintPost();
+
+            newPost.UpVote();
+            newPost.UpVote();
+            newPost.UpVote();
+            newPost.UpVote();
+            newPost.UpVote();
+
+            newPost.PrintPost();
+
+            newPost.DownVote();
+            newPost.DownVote();
+            newPost.DownVote();
+            
+            newPost.PrintPost();
+
+            Console.WriteLine("Should throw exception - about to try to go into negative votes");
+            newPost.DownVote();
+            newPost.DownVote();
+            newPost.DownVote();
+            
+
+
+        }
+
+    }
+}
